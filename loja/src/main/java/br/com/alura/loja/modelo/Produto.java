5a11df7cc9d32ab954ba8a5a531251e528663a73
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-12 22:09:56 -0300

    Persistência com JPA: Hibernate
    Persistindo uma entidade

--- loja/src/main/java/br/com/alura/loja/modelo/Produto.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -9,9 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "produtos")
+@Table(name = "produtos")
 public class Produto {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -20,5 +20,33 @@ public class Produto {
 	private BigDecimal preco;
 	
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
+
+	
+
+	
 }
