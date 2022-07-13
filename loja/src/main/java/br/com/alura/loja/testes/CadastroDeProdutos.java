4da5120bee9d0e15b71a2a0b5730aef533ca007d
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-13 08:42:17 -0300

    Persistência com JPA: Hibernate Persistência com JPA: Hibernate
    Mais mapeamentos

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -4,16 +4,16 @@ import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi Redmi");
-		celular.setDescricao("Muito Legal");
-		celular.setPreco(new BigDecimal("800"));
+		Produto celular = new Produto("Xiaomi Redmi", "Muito Legal", 
+				new BigDecimal("800"), Categoria.CELULARES);
+		
 
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao dao = new ProdutoDao(em);
