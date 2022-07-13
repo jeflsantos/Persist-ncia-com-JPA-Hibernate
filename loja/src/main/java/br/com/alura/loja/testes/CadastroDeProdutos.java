3576c750e8781057394e85881f98f64c782bae25
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-13 08:19:17 -0300

    Update CadastroDeProdutos.java

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -3,10 +3,9 @@ package br.com.alura.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
 
@@ -15,12 +14,12 @@ public class CadastroDeProdutos {
 		celular.setNome("Xiaomi Redmi");
 		celular.setDescricao("Muito Legal");
 		celular.setPreco(new BigDecimal("800"));
-		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-		EntityManager em = factory.createEntityManager();
+
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao dao = new ProdutoDao(em);
 		
 		em.getTransaction().begin();
-		em.persist(celular); 
+		dao.cadastrar(celular); 
 		em.getTransaction().commit();
 		em.close();
 
